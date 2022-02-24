e05ef8d88b16fe6eaf6e5911b134881d30b62f53
jdh4 <52128661+jdh4@users.noreply.github.com>
2022-02-24 11:10:15 -0500

    Update mnist_classify.py

--- 01_single_gpu/mnist_classify.py
+++ 01_single_gpu/mnist_classify.py
@@ -36,7 +36,7 @@ class Net(nn.Module):
         output = F.log_softmax(x, dim=1)
         return output
 
-#@profile
+@profile
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):

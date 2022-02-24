fa8ec61efcf6d0301773c37743cfa42630ee9925
jdh4 <52128661+jdh4@users.noreply.github.com>
2022-02-23 19:09:25 -0500

    Create download_cifar10.py

---
+++ 03_pytorch_lightning/multi/download_cifar10.py
@@ -0,0 +1,9 @@
+import torchvision
+import warnings
+warnings.filterwarnings("ignore")
+
+# compute nodes do not have internet access so download the data before
+# submitting the job
+
+_ = torchvision.datasets.CIFAR10('data', train=True, transform=None,
+                                 target_transform=None, download=True)

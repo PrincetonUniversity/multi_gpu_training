f966d2b2162f3e3f7a9341beea5ab685935cc531
jdh4 <52128661+jdh4@users.noreply.github.com>
2022-02-24 15:39:25 -0500

    Update download_cifar10.py

--- 03_pytorch_lightning/multi/download_cifar10.py
+++ 03_pytorch_lightning/multi/download_cifar10.py
@@ -5,5 +5,5 @@ warnings.filterwarnings("ignore")
 # compute nodes do not have internet access so download the data before
 # submitting the job
 
-_ = torchvision.datasets.CIFAR10('data', train=True, transform=None,
+_ = torchvision.datasets.CIFAR10('.', train=True, transform=None,
                                  target_transform=None, download=True)

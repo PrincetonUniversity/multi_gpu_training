f9a856cac1a0a130fb7d5671cc8ea0b3ea2d87cf
jdh4 <52128661+jdh4@users.noreply.github.com>
2022-02-24 15:40:31 -0500

    Create myscript.py

---
+++ 03_pytorch_lightning/multi/myscript.py
@@ -0,0 +1,124 @@
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from pl_bolts.datamodules import CIFAR10DataModule
+from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
+from pytorch_lightning import LightningModule, Trainer, seed_everything
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch.optim.lr_scheduler import OneCycleLR
+from torch.optim.swa_utils import AveragedModel, update_bn
+from torchmetrics.functional import accuracy
+
+seed_everything(7)
+
+BATCH_SIZE = 256
+PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
+NUM_WORKERS   = int(os.environ["SLURM_CPUS_PER_TASK"])
+NUM_NODES     = int(os.environ["SLURM_NNODES"])
+GPUS_PER_NODE = int(os.environ["SLURM_GPUS_ON_NODE"])
+
+train_transforms = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.RandomCrop(32, padding=4),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ToTensor(),
+        cifar10_normalization(),
+    ]
+)
+
+test_transforms = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.ToTensor(),
+        cifar10_normalization(),
+    ]
+)
+
+cifar10_dm = CIFAR10DataModule(
+    data_dir=PATH_DATASETS,
+    batch_size=BATCH_SIZE,
+    num_workers=NUM_WORKERS,
+    train_transforms=train_transforms,
+    test_transforms=test_transforms,
+    val_transforms=test_transforms,
+)
+
+def create_model():
+    model = torchvision.models.resnet18(pretrained=False, num_classes=10)
+    model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+    model.maxpool = nn.Identity()
+    return model
+
+class LitResnet(LightningModule):
+    def __init__(self, lr=0.05):
+        super().__init__()
+
+        self.save_hyperparameters()
+        self.model = create_model()
+
+    def forward(self, x):
+        out = self.model(x)
+        return F.log_softmax(out, dim=1)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def evaluate(self, batch, stage=None):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = accuracy(preds, y)
+
+        if stage:
+            self.log(f"{stage}_loss", loss, prog_bar=True)
+            self.log(f"{stage}_acc", acc, prog_bar=True)
+
+    def validation_step(self, batch, batch_idx):
+        self.evaluate(batch, "val")
+
+    def test_step(self, batch, batch_idx):
+        self.evaluate(batch, "test")
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(
+            self.parameters(),
+            lr=self.hparams.lr,
+            momentum=0.9,
+            weight_decay=5e-4,
+        )
+        steps_per_epoch = 45000 // BATCH_SIZE
+        scheduler_dict = {
+            "scheduler": OneCycleLR(
+                optimizer,
+                0.1,
+                epochs=self.trainer.max_epochs,
+                steps_per_epoch=steps_per_epoch,
+            ),
+            "interval": "step",
+        }
+        return {"optimizer": optimizer, "lr_scheduler": scheduler_dict}
+
+model = LitResnet(lr=0.05)
+model.datamodule = cifar10_dm
+
+trainer = Trainer(
+    gpus=GPUS_PER_NODE,
+    num_nodes=NUM_NODES,
+    strategy='ddp',
+    precision=32,
+    max_epochs=10,
+    progress_bar_refresh_rate=10,
+    logger=TensorBoardLogger("lightning_logs/", name="resnet"),
+    callbacks=[LearningRateMonitor(logging_interval="step")],
+)
+
+trainer.fit(model, cifar10_dm)
+trainer.test(model, datamodule=cifar10_dm)

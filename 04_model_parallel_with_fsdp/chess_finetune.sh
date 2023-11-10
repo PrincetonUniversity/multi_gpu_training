270a01da551a6b97f6594ad29b306dcdf5ea0ff4
jdh4 <52128661+jdh4@users.noreply.github.com>
2023-11-10 16:32:48 -0500

    Update chess_finetune.sh

--- 04_model_parallel_with_fsdp/chess_finetune.sh
+++ 04_model_parallel_with_fsdp/chess_finetune.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -l
-#SBATCH -J chess_finetune
-#SBATCH -N 1
-#SBATCH --output=%x-%j_%n.out
-#SBATCH --gres=gpu:a100:4
-#SBATCH --constraint gpu80
-#SBATCH -c 4
-#SBATCH --mem=32G
-#SBATCH -t 0-1
+#SBATCH --job-name=chess_finetune  # create a short name for your job
+#SBATCH --output=%x-%j_%n.out      # file to write stdout
+#SBATCH --nodes=1                  # node count
+#SBATCH --ntasks=1                 # total number of tasks across all nodes
+#SBATCH --cpus-per-task=4          # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem=256G                 # cpu memory per node
+#SBATCH --gres=gpu:4               # number of gpus per node
+#SBATCH --time=01:00:00            # total run time limit (HH:MM:SS)
+#SBATCH --constraint=gpu80         # run job on specific node types
 
 total_batch_size=${TOTAL_BATCH_SIZE:-64} # total batch size per optimization
 batch_size_per_device=${BATCH_SIZE_PER_DEVICE:-1} # batch size per device
@@ -14,10 +15,8 @@ batch_size_per_device=${BATCH_SIZE_PER_DEVICE:-1} # batch size per device
 num_gpus=$(jq -n "[$CUDA_VISIBLE_DEVICES] | length")
 gradient_accumulation_steps=$(($total_batch_size / $batch_size_per_device / $num_gpus))
 
-
-free_port=$(comm -23 <(seq 49152 65535 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1)
 torchrun \
-    --nnodes=1 \
+    --nnodes=${SLURM_NNODES} \
     --nproc-per-node=$num_gpus \
     chess_finetune.py \
         --batch_size_per_device $batch_size_per_device \
